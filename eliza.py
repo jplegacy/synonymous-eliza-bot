08cbf761e3e37d2bec96b38cf1d8f2436e076dbe
jplegacy <jeremyperez1122@gmail.com>
2023-09-09 16:35:18 -0400

    Updated Responses and Extended COnversation Replies added

--- eliza.py
+++ eliza.py
@@ -3,72 +3,118 @@ import random
 import sys
 
 class ChatAgent: #replace this with your last name, like SchlueterChatAgent
-   """ChatAgent - This is a very simple ELIZA-like computer program in python.
+    """ChatAgent - This is a very simple ELIZA-like computer program in python.
       Your assignent in Programming Assignment 1 is to improve upon it.
 
       I've created this as a python object so that your agents can chat with one another
       (and also so you can have some practice with python objects)
       """
-  
    
-   def generateReply(self,inString):
+    def generateReply(self,inString):
        """Pick a random function, and call it on the input string """
        randFunction = random.choice(self.ReplyFunctionList) #pick a random function, I love python
        reply = randFunction(inString) 
        return reply
  
-
-   def driverLoop(self):
+    def driverLoop(self):
        """The main driver loop for the chat agent"""
        reply = "how are you today?"
+       self.historyResponse.append(reply)
+
+       
        while True:
            response = input(reply)
-           reply = self.generateReply(response)
-           #print(reply)
+           self.historyInput.append(response)
 
-   def swapPerson(self,inWord):
+           reply = self.generateReply(response)
+           self.historyResponse.append(reply)
+
+           self.recordDialog(response,reply)
+
+           #print(reply)
+           
+    def recordDialog(self, inputs, output):
+        self.historyInput.append(inputs)
+        self.historyResponse.append(output)
+        
+        if not self.longConversation  and len(self.historyResponse) == self.MAXHISTORYMEMORY:
+            self.longConversation = True
+            self.ReplyFunctionList.append(self.reflectiveFeedback)
+            self.ReplyFunctionList.append(self.elaborateDifferently)
+            
+        if len(self.historyResponse) == self.MAXHISTORYMEMORY:
+            self.historyInput.pop(0)
+            self.historyResponse.pop(0)
+            
+    def swapPersonTerm(self,inWord):
        """Replace 'I' with 'You', etc"""
        if (inWord in self.PronounDictionary.keys()):   #if the word is in the list of keys
            return self.PronounDictionary[inWord] 
        else:
            return inWord
 
-
-   def changePerson(self,inString): #this function is deliberately awful. fix it.
+    
+    def switchPerson(self,inString):
        """change the pronouns etc of inString
        by iterating through the PrononDictionary
        and substituting keywords for subwords
-
-       n.b. this is an absolutely awful way of doing this
-       and you'll be asked to change it in the assignment"""
+       """
+       
        inWordList = str.split(inString)
-       newWordList = [] 
-       for keyword, subword in self.PronounDictionary.items(): #iterate through the dictionary
-           for word in inWordList:  #and then through the sentence
-               if (word == keyword):
-                   newWordList.append(subword)  #replace if it matches
-               else:
-                   newWordList.append(word)
-           inWordList,newWordList = newWordList,[]
-       reply = ' '.join(inWordList)  #glue things back together
+       switched = map(self.swapPersonTerm, inWordList)
+
+       reply = ' '.join(switched)  #glue things back together
        return reply 
 
-   def changePersonAndAddPrefix(self,inString):
-        reply = self.changePerson(inString)
+    def changePersonAndAddPrefix(self,inString):
+        reply = self.switchPerson(inString)
         randomPrefix = random.choice(self.PrefixList)
         return ''.join([randomPrefix,reply])
 
   
-   def generateHedge(self,inString):
+    def generateHedge(self,inString):
         return random.choice(self.HedgeList);
 
+    def reflectiveFeedback(self,inString):
+        selfReflectQuestion = random.choice(self.selfReflectQuestionList)
+        randomResponse = random.choice(self.historyResponse)
 
-   def __init__(self):
-       self.PronounDictionary = {'i':'you','I':'you','am':'are'}
-       self.HedgeList = ["Hmm","That is fascinating","Let's change the subject", "Perhaps", "To some extent", "Sometimes"]
-       self.PrefixList = ["Why do you say that ","What do you mean that ", "Well why wouldn't ", "Are you sure that ", "Could you explain what you meant by "]
+        return ''.join([selfReflectQuestion, randomResponse])
+   
+    def elaborateDifferently(self,inString):
+        explainQuestion = random.choice(self.explainPreviousList)
+        randomInput = random.choice(self.historyInput[:self.MAXHISTORYMEMORY-2]) #wont include the last response
+        question = random.choice(self.questions) 
+
+        return ''.join([explainQuestion, randomInput,question])
+    
+    
+    def interstingTerm(self, inString):
+        inWordList = str.split(inString)
+        longest = max(inWordList, key=len)
+        
+        return longest
+
+            
+    def __init__(self):
        
-       self.ReplyFunctionList = [self.generateHedge,self.changePerson,self.changePersonAndAddPrefix] #this is what makes Python so powerful
+       self.MAXHISTORYMEMORY = 10 #the number of responses that the bot will remember 
+
+       self.PronounDictionary = {'i':'you','I':'you','am':'are', 'you':'I', 'me':'you'}
+       self.HedgeList = ["Hmm","That is fascinating","Let's change the subject", "Perhaps", "To some extent", "Sometimes"]
+       self.PrefixList = ["Why do you say that ","What do you mean that ", "Well why wouldn't you say ", "Are you sure that ", "Could you explain what you meant by ", "What do you think of "]
+              
+       self.selfReflectQuestionList = ["how did it affect you earlier when I said ", "how did you feel after I said "]
+       self.historyResponse = []
+
+       self.explainPreviousList = ["you previously said "]
+       self.historyInput = []
+       self.questions = [", how does this change things", ", how does this affect your life"]
+
+       self.ReplyFunctionList = [self.generateHedge,self.changePersonAndAddPrefix] #this is what makes Python so powerful
+   
+       self.longConversation = False
+
    #End of ChatAgent
 
 if __name__ == '__main__': #will only be called if this is invoked directly by python, as opposed to included in a larger file
